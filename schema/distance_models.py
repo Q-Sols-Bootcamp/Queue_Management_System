17095e4d3335bebfc2e24f8632fb7dba34e9d42d
alishan-khowaja <qsolakhowaja9@gmail.com>
2024-10-09 14:11:03 +0500

    Update distance_models.py

--- schema/distance_models.py
+++ schema/distance_models.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class Location(BaseModel):
     latitude: float 
     longitude: float
+
+class UpdateEtaReaquest(BaseModel):
+    userid: int
+    location: Location 
